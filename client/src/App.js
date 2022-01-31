1385d678b5587d78b607c5fe4f9b72dae928fc76
Dmitry Mumrenkov <dimony4519@gmail.com>
2022-01-31 22:31:59 +0300

    Longpulling chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
+import React from "react";
+import LongPulling from "./LongPulling";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <LongPulling />
     </div>
   );
 }
